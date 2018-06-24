4b548393911abd7e6fc5bb6fbfff6d6d22a00e18
maestroi <Demonboy90>
2018-06-24 16:10:41 +0200

    inimiq

--- scripts/iNimiqpool-Install-Script-V0.3.2.sh
+++ scripts/iNimiqpool-Install-Script-V0.3.2.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "  iNimiq mining setup                "
+echo "  inimiq mining setup                "
 echo " Credits to sushipool                "
 echo ""
 echo
@@ -16,11 +16,11 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update && sudo apt-get install -y yarn
 
 if [ -d "source" ]; then
-    echo 'Existing source directory found. Updating to latest iNimiq codes.'
+    echo 'Existing source directory found. Updating to latest inimiq codes.'
     cd source/miner
     git pull
 else
-    echo 'Cloning the latest iNimiq codes.'
+    echo 'Cloning the latest inimiq codes.'
     git clone https://github.com/inimiqpool/source.git
     cd source/miner
 fi
@@ -31,7 +31,7 @@ NC='\033[0m' # No Color
 echo 'startup_message off' >> ~/.screenrc
 
 ans=`DIALOG_ERROR=5 DIALOG_ESC=1 dialog --timeout 120 \
-           --menu "Do you wish to start the iNimiq miner? (this message will time-out in 120s.)" 20 73 8 \
+           --menu "Do you wish to start the inimiq miner? (this message will time-out in 120s.)" 20 73 8 \
            "1) Yes" "Run miner." \
            "2) Yes, in background" "Run miner using screen." \
            "3) No" "Quit installation." \
@@ -41,28 +41,28 @@ case $rc in
    0) case "$ans" in
         "1) Yes")
             printf "\033c"
-            echo -e "Starting iNimiq miner."
-            ./iNimiq
+            echo -e "Starting inimiq miner."
+            ./inimiq
             exit;;
         "2) Yes, in background")
             printf "\033c"
-            echo -e "Starting iNimiq miner in a screen session."
+            echo -e "Starting inimiq miner in a screen session."
             echo "To detach a screen session and return to your normal SSH terminal, type CTRL+A D"
             echo "You can also close your SSH now, and the miner will continue running."
             echo -e "To return to the miner next time, type ${RED}screen -x${NC}."
             read -n 1 -s -r -p "Press any key to continue."
-            screen ./iNimiq
+            screen ./inimiq
             exit;;
         "3) No")
             echo -e "Installation finished. To start mining, type:"
             echo -e "${RED}$ cd source/miner${NC}"
-            echo -e "${RED}$ ./iNimiq${NC}"
+            echo -e "${RED}$ ./inimiq${NC}"
             echo
             exit;;
       esac;;
    *)
        printf "\033c"
-       echo -e "Starting iNimiq miner."
-       ./iNimiq
+       echo -e "Starting inimiq miner."
+       ./inimiq
       exit;;
 esac
